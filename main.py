7eada57981be8890e0c8d5cae11ae80fb4a3bd50
LukeCake <80320893+LukeCake@users.noreply.github.com>
2022-10-10 00:13:48 +0200

    0

---
+++ main.py
@@ -0,0 +1,14 @@
+#dayZ
+
+
+# in command prompt, type "pip install pynput" to install pynput.
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard = Controller()
+time.sleep(5)
+for i in  range()
+    key = "a"
+
+keyboard.press(key)
+keyboard.release(key)
